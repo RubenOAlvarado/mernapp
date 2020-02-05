af0b88e3b0cf4b744d04f25f36580aa3def17434
RubenOAlvarado <rubenosmaralvarado@gmail.com>
2020-02-04 18:59:32 -0600

    Backend ready, next frontend

--- backend/api/routes/routes.js
+++ backend/api/routes/routes.js
@@ -1,7 +1,17 @@
-const mongoose = require('mongoose'),
-    express = require('express'),
+const express = require('express'),
     router = express.Router();
 
+//importamos el controlador
+const controller = require('../controllers/student-controller');
 
-const studentSchema = require('../Models/Student');
+router.post('/create-student', controller.createStudent);
 
+router.get('/', controller.readStudents);
+
+router.get('/edit-student/:id', controller.getStudent);
+
+router.put('/update-student/:id', controller.updateStudent);
+
+router.delete('/delete-student/:id', controller.deleteStudent);
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,50 @@
+const express = require('express'),
+    mongoose = require('mongoose'),
+    cors = require('cors'),
+    logger = require('morgan'),
+    createError = require('http-errors'),
+    PORT = process.env.PORT || 3000,
+    bodyParser = require('body-parser');
+
+
+const API_PORT = 3001;
+const app = express();
+app.use(cors());
+
+//this is our mongo database
+const dbRoute = 'mongodb+srv://Munditoro:munditoro@crudtest-h3y3e.mongodb.net/students?retryWrites=true&w=majority';
+
+//connects our backend code with the database
+mongoose.connect(dbRoute, { useNewUrlParser: true });
+
+let db = mongoose.connection;
+
+db.on('open', () => console.log('connected to the database'));
+
+//checks if the connection with the database was successful
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+const routes = require('./api/routes/routes');
+app.use('/', routes);
+
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//catch 404 and forward to error handler
+app.use((req, res, next) => next(createError(404)));
+
+//Error Handler
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.send('error');
+});
+
+app.listen(PORT);
+
+console.log('App listening in port: ' + PORT);
